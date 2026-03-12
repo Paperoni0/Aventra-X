44357a7d41b09093fbd7798e1e1d25de5da09760
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-03-12 22:02:36 +0700

    Delete middleware.js

--- middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(req) {
-    const host = req.headers.get("host");
-    if (host?.includes("paperoni0.github.io")) {
-        return NextResponse.redirect("https://batch9mw.qzz.io/xa");
-    }
-    return NextResponse.next();
-}
