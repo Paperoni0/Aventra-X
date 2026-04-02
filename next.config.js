73afa294f3ee3618c823a036128c367f6627193b
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-04-02 20:11:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /**
  * @type {import('next').NextConfig}
  */
-const isProd = process.env.NODE_ENV = 'production';
-
 const nextConfig = {
-  basePath: isProd ? '/xa' : '',
-  assetPrefix: isProd ? '/xa' : '',
   output: 'export',
   distDir: 'dist',
   images: {
